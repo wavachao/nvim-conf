360345f40743ee3c7b27eb99f4d44303d8ee50f1
hchao <huachaowu@qq.com>
2025-09-23 16:33:39 +0800

    ✨ feat(autocmds.lua)：添加自动保存功能，在失去焦点或离开缓冲区时自动保存修改

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,24 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Auto-save on focus change (like VSCode's onFocusChange)
+vim.api.nvim_create_autocmd("FocusLost", {
+  desc = "Auto-save when losing focus",
+  pattern = "*",
+  callback = function()
+    if vim.bo.modified and not vim.bo.readonly and vim.fn.expand("%") ~= "" then
+      vim.cmd("silent! update")
+    end
+  end,
+})
+
+vim.api.nvim_create_autocmd("BufLeave", {
+  desc = "Auto-save when leaving buffer",
+  pattern = "*",
+  callback = function()
+    if vim.bo.modified and not vim.bo.readonly and vim.fn.expand("%") ~= "" then
+      vim.cmd("silent! update")
+    end
+  end,
+})
